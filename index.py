a6f807d2ad37a1373c1a2aa7a7d7636763b1158b
Harald Schilly <hsy@sagemath.com>
2015-05-16 11:45:09 +0000

    index.py: better formatting, indexing sub-pdfs (without filtering) to capture the individual reference PDFs, sorting of entries

--- index.py
+++ index.py
@@ -4,11 +4,28 @@ from glob import glob
 from collections import defaultdict
 
 LANG = {
-    "en": "english",
-  "hu": "hungarian"
+    "en": "English",
+  "hu": "Hungarian",
+  "pt": "Portuguese",
+  "de": "German",
+  "ru": "Russian",
+  "fr": "French",
+  "it": "Italian",
+  "ca": "Catalan"
 }
 
-output = ["<!DOCTYPE html><body>"]
+output = ["""\
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="html/en/website/_static/sage.css" type="text/css" />
+<style>
+html {margin: 20px; }
+body { background-color: white; }
+</style>
+<link rel="shortcut icon" href="html/en/website/_static/favicon.ico"/>
+</head>
+<body>
+"""]
 
 pages = {"html": defaultdict(list), "pdf": defaultdict(list)}
 
@@ -16,24 +33,38 @@ for path in glob("*/*"):
     what, lang = path.split("/")
 
     for path2 in glob(path + "/*"):
-        EXT = "/*.pdf" if what == "pdf" else "/index.html"
-        for fn in glob(path2 + EXT):
-            pages[what][lang].append(fn)
+        if what == "html":
+            EXT = "/index.html"
+            for fn in glob(path2 + EXT):
+                pages[what][lang].append(fn)
+        elif what == "pdf":
+            for EXT in ["/*.pdf", "/*/*.pdf"]:
+                for fn in glob(path2 + EXT):
+                    pages[what][lang].append(fn)
+
 
 output.append("<h1>Documentation</h1>")
 
-for what in pages.keys():
+for what in sorted(pages.keys()):
     output.append("<h2>%s</h2>" % what.upper())
     output.append("<ul>")
-    for lang, entries in pages[what].iteritems():
+    for lang, entries in sorted(pages[what].iteritems()):
         output.append("<li>%s</li>" % LANG.get(lang, lang))
         output.append("<ul>")
         for entry in entries:
+            if what == "html":
+                fn = entry.split("/")[-2].replace("_", " ").title()
+            elif what == "pdf":
+                fn = entry.split(
+                    "/")[-1][:-4].replace("_",
+                                          " ").title() + " (PDF)"
             output.append("<li><a href='{path}'>{fn}</a></li>".format(
-                          path=entry, lang=LANG.get(lang, lang), fn="/".join(entry.split("/")[-2:])))
+                          path=entry,
+                          lang=LANG.get(lang, lang),
+                          fn=fn))
         output.append("</ul>")
     output.append("</ul>")
-    output.append("</body>")
+    output.append("</body></html>")
 
 with open("index.html", "w") as index:
     index.write("\n".join(output))
