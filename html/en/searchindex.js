d7a518ac878bfdf64888b8af54ae9907ee081207
Harald Schilly <harald.schilly@gmail.com>
2016-08-05 07:40:05 +0000

    7.3 release

--- html/en/searchindex.js
+++ html/en/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{edit:0,file:0,output:0,overridden:0,templat:0,thi:0},titles:["Sage Documentation"],titleterms:{document:0,sage:0}})
+Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{edit:0,file:0,output:0,overridden:0,templat:0,thi:0},titles:["Sage Documentation"],titleterms:{document:0,sage:0}})
