5d1aa1f84ef53868098df2ac3930994f983101b5
Harald Schilly <harald.schilly@gmail.com>
2018-12-28 11:00:24 +0000

    8.5 take 2

--- html/en/reference/graphs/sage/graphs/graph_plot-21.py
+++ html/en/reference/graphs/sage/graphs/graph_plot-21.py
@@ -1,4 +1,4 @@
-g = Graph({}, loops=True, multiedges=True, sparse=True)
+g = Graph(loops=True, multiedges=True, sparse=True)
 g.add_edges([(0,0,'a'),(0,0,'b'),(0,1,'c'),(0,1,'d'),
     (0,1,'e'),(0,1,'f'),(0,1,'f'),(2,1,'g'),(2,2,'h')])
 GP = g.graphplot(vertex_size=100, edge_labels=True,
--- html/en/reference/graphs/sage/graphs/graph_plot-24.py
+++ html/en/reference/graphs/sage/graphs/graph_plot-24.py
@@ -1,4 +1,4 @@
-d = DiGraph({}, loops=True, multiedges=True, sparse=True)
+d = DiGraph(loops=True, multiedges=True, sparse=True)
 d.add_edges([(0,0,'a'),(0,0,'b'),(0,1,'c'),(0,1,'d'),
     (0,1,'e'),(0,1,'f'),(0,1,'f'),(2,1,'g'),(2,2,'h')])
 GP = d.graphplot(vertex_size=100, edge_labels=True, color_by_label=True,
