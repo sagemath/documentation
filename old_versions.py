796ed78056e3f8848813a852771587aa074990b0
Harald Schilly <harald.schilly@gmail.com>
2021-08-25 09:35:30 +0000

    finishing collecting older versions

--- old_versions.py
+++ old_versions.py
@@ -2,14 +2,33 @@
 
 import os
 
-# created by the CI script, to checkout workspaces at older tags
+# this checks out workspaces at older tags into ./public/
+# used by gitlab_ci.py (or other deployment scripts)
 # make sure to push those tags to the target repo!
 OLD_VERSIONS = [
-    #'6.7',
-    #'6.8',
-    #'8.8',
-    #'8.9',
-    #'9.0',
+    '6.6',
+    '6.7',
+    '6.8',
+    "6.9",
+    "6.10",
+    "7.0",
+    "7.1",
+    '7.2',
+    '7.3',
+    '7.4',
+    '7.5',
+    '7.6',
+    '8.0',
+    '8.1',
+    '8.2',
+    '8.3',
+    '8.4',
+    '8.5',
+    '8.6',
+    '8.7',
+    '8.8',
+    '8.9',
+    '9.0',
     '9.1',
     '9.2',
     '9.3',
